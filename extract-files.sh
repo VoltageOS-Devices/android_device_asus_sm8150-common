1c9331a2f7e88525a79c9fc7c440332a78b7e1bc
Jackeagle <jackeagle102@gmail.com>
2022-09-17 23:04:29 -0400

    sm8150-common: Patch libvendor.goodix.hardware.biometrics.fingerprint@2.1.so to use libhidlbase from vndk 32
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -60,6 +60,10 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+    # Use VNDK 32 libhidlbase
+    vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+        "${PATCHELF_0_8}" --remove-needed "libhidlbase.so" "${2}"
+        sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
     ;;
     esac
 }
