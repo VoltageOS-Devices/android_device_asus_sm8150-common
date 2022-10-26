ce477c94f176c3cd4577a284970545cec396a5f3
Jackeagle <jackeagle102@gmail.com>
2022-10-26 12:22:50 +0200

    sm8150-common: Build the QTI power HAL
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -362,6 +362,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
