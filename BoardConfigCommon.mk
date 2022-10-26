3538ab4874f40aa1ca37950e1054c88f17b5ae05
Jackeagle <jackeagle102@gmail.com>
2022-10-26 07:52:20 +0200

    sm8150-common: Build vbmeta with a disabled hashtree
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,5 +52,10 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 # Inherit from the proprietary version
 include vendor/asus/sm8150-common/BoardConfigVendor.mk
