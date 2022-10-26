9efd1683a4d657a25346460b4d97d02ff10d0018
Jackeagle <jackeagle102@gmail.com>
2022-10-26 11:51:01 +0200

    sm8150-common: Build rmnet control library
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -162,6 +162,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Data
+PRODUCT_PACKAGES += \
+    librmnetctl
+
 # Display
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
