a200dbc3385cf511848eb93b0f27e3c5fbd30f01
Jackeagle <jackeagle102@gmail.com>
2022-10-26 12:01:47 +0200

    sm8150-common: Import sec_config from asus
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -307,6 +307,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
