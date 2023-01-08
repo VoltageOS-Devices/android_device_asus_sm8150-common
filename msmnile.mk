5c60d42d2cd2f5ae767148791e9b85eeeac461c6
Jackeagle <jackeagle102@gmail.com>
2023-01-08 13:21:12 -0500

    sm8150-common: init: Drop init.set_kptr.rc
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -315,7 +315,6 @@ PRODUCT_PACKAGES += \
     init.asus.rc \
     init.qcom.factory.rc \
     init.qcom.rc \
-    init.set_kptr.rc \
     init.target.rc \
     init.qcom.power.rc \
     init.qcom.usb.rc \
